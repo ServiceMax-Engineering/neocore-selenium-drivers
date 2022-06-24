af38045e8f448410a90f5a33a4fdcb15bdc6bb6e
maxio <maxio@servicemax.com>
2022-06-24 13:50:07 +0000

    upgrade chrome driver to 103.0.5060.53

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   version: '3.141.59',
   drivers: {
     chrome: {
-      version: '102.0.5005.61',
+      version: '103.0.5060.53',
       arch: process.arch,
       baseURL: 'https://chromedriver.storage.googleapis.com'
     },
@@ -19,7 +19,7 @@ module.exports = {
       baseURL: 'https://github.com/mozilla/geckodriver/releases/download'
     },
     chromiumedge: {
-      version: '102.0.1245.33',
+      version: '102.0.1245.44',
       arch: process.arch,
       baseURL: 'https://msedgedriver.azureedge.net',
     },
